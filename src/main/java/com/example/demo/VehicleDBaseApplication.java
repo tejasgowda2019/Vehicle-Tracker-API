d32935f474eb9088485e75a353470d3ca36b808d
tejasgowda2019 <tejasgowda2019@gmail.com>
2023-12-07 10:43:03 +0530

    Adding vehicleDb Project

---
+++ src/main/java/com/example/demo/VehicleDBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleDBaseApplication.class, args);
+	}
+
+}
